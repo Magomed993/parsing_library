94a33b0aed1554034435e81a8b404d1e6144060c
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-11-14 22:47:53 +0300

    Made it possible to choose Made it orderly

--- tululu.py
+++ tululu.py
@@ -1,9 +1,10 @@
 import requests
 import os
+import argparse
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-from urllib.parse import urlparse, urlsplit, unquote
+from urllib.parse import urlparse
 
 
 def check_for_redirect(response):
@@ -54,8 +55,6 @@ def parse_book_page(url):
     title = tag[0].strip()
     author = tag[1].strip()
     site_integration = urljoin(book_site, img)
-    # link_parse = urlparse(site_integration)
-    # path_separation = os.path.splitext(link_parse.path)
     comment_lst = []
     for comment in comment_tags:
         comment_lst.append(comment.find('span').text)
@@ -77,41 +76,27 @@ def main():
     directory_img = 'image/'
     os.makedirs(directory_books, exist_ok=True)
     os.makedirs(directory_img, exist_ok=True)
-    book_site = 'https://tululu.org/'
+    argument_parse = argparse.ArgumentParser(description="""Скачивает книгу в количестве по значению установленному по умолчанию.
+Прописав аргументы даёт возможность установить количество книг необходимое для скачивания.
+    """)
+    argument_parse.add_argument('start_id', default=1, nargs='?', type=int, help='Начало цикла')
+    argument_parse.add_argument('end_id', default=11, nargs='?', type=int, help='Конец цикла')
+    args = argument_parse.parse_args()
 
-    for i in range(1, 11):
+    for i in range(args.start_id, args.end_id):
         url_download = f"https://tululu.org/txt.php?id={i}"
         url_book = f'https://tululu.org/b{i}/'
-
         try:
-            response = requests.get(url_book)
-            response.raise_for_status()
-            check_for_redirect(response)
-            soup = BeautifulSoup(response.text, 'lxml')
-            tag = soup.find('h1').text.split('::')
-            img = soup.find('div', class_='bookimage').find('img')['src']
-            comment_tags = soup.find_all('div', class_='texts')
-            genre_tag = soup.find('span', class_='d_book').find_all('a')
-            title = tag[0].strip()
-            author = tag[1].strip()
-            filename = f"{i}. {title} - {author}"
-            site_integration = urljoin(book_site, img)
-            link_parse = urlparse(site_integration)
+            data_book = parse_book_page(url_book)
+            filename = f"{i}. {data_book['Название']} - {data_book['Автор']}"
+            link_parse = urlparse(data_book['Адрес картинки'])
             path_separation = os.path.splitext(link_parse.path)
-            # download_image(site_integration, f'{i}{path_separation[-1]}', directory_img)
-            # download_txt(url_download, filename, directory_books)
-            # print(f'Заголовок: {title}')
-            # genre_lst = []
-            # for i in genre_tag:
-            #     genre_lst.append(i.text)
-            # print(genre_lst)
-            print(parse_book_page(url_book))
-            # print(urljoin(book_site, img))
-            # print()
-            # for comment in comment_tags:
-            #     print(comment.find('span').text)
-        except requests.exceptions.HTTPError as http_error:
-            print(f'Ошибка на ID {i}: {http_error}')
+            download_image(data_book['Адрес картинки'], f'{i}{path_separation[-1]}', directory_img)
+            download_txt(url_download, filename, directory_books)
+            print('Название: ', data_book['Название'])
+            print('Автор: ', data_book['Автор'])
+            print()
+        except requests.exceptions.HTTPError:
             continue
 
 if __name__ == '__main__':
