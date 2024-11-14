f43a7f5cd2657ee59883a31552612b67743ab49c
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-11-14 20:59:58 +0300

    Added a function

--- tululu.py
+++ tululu.py
@@ -41,6 +41,37 @@ def download_image(url, filename, folder='image/'):
     return directory
 
 
+def parse_book_page(url):
+    book_site = 'https://tululu.org/'
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    soup = BeautifulSoup(response.text, 'lxml')
+    tag = soup.find('h1').text.split('::')
+    img = soup.find('div', class_='bookimage').find('img')['src']
+    comment_tags = soup.find_all('div', class_='texts')
+    genre_tag = soup.find('span', class_='d_book').find_all('a')
+    title = tag[0].strip()
+    author = tag[1].strip()
+    site_integration = urljoin(book_site, img)
+    # link_parse = urlparse(site_integration)
+    # path_separation = os.path.splitext(link_parse.path)
+    comment_lst = []
+    for comment in comment_tags:
+        comment_lst.append(comment.find('span').text)
+    genre_lst = []
+    for genre in genre_tag:
+        genre_lst.append(genre.text)
+    site_data = {
+        'Название': title,
+        'Автор': author,
+        'Адрес картинки': site_integration,
+        'Комментарии': comment_lst,
+        'Жанр': genre_lst
+    }
+    return site_data
+
+
 def main():
     directory_books = 'books/'
     directory_img = 'image/'
@@ -69,12 +100,12 @@ def main():
             path_separation = os.path.splitext(link_parse.path)
             # download_image(site_integration, f'{i}{path_separation[-1]}', directory_img)
             # download_txt(url_download, filename, directory_books)
-            print(f'Заголовок: {title}')
-            genre_lst = []
-            for i in genre_tag:
-                genre_lst.append(i.text)
-            print(genre_lst)
-
+            # print(f'Заголовок: {title}')
+            # genre_lst = []
+            # for i in genre_tag:
+            #     genre_lst.append(i.text)
+            # print(genre_lst)
+            print(parse_book_page(url_book))
             # print(urljoin(book_site, img))
             # print()
             # for comment in comment_tags:
