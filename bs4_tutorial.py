1023e84dbe99f1ed15d06de7fdc0a026a1afcdbd
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-11-15 09:22:54 +0300

    Added README Corrected the names of variables Added docstring

--- bs4_tutorial.py
+++ bs4_tutorial.py
@@ -2,16 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# url = 'https://www.franksonnenbergonline.com/blog/are-you-grateful/'
-url = 'https://tululu.org/b9/'
+url = 'https://www.franksonnenbergonline.com/blog/are-you-grateful/'
 response = requests.get(url)
 response.raise_for_status()
 
 soup = BeautifulSoup(response.text, 'lxml')
-# title_tag = soup.find('main').find('header').find('h1').text
-# img_tag = soup.find('img', class_='attachment-post-image')['src']
-# text_tag = soup.find('main').find('div', class_='entry-content').find('p').text
-# print(f'{title_tag}\n{img_tag}\n\n{text_tag}')
-comment_tag = soup.find_all('div', class_='texts')
-for i in comment_tag:
-    print(i.find('span').text)
+tag_title = soup.find('main').find('header').find('h1').text
+tag_img = soup.find('img', class_='attachment-post-image')['src']
+tag_text = soup.find('main').find('div', class_='entry-content').find('p').text
+print(f'{tag_title}\n{tag_img}\n\n{tag_text}')
--- tululu.py
+++ tululu.py
@@ -32,6 +32,14 @@ def download_txt(url, filename, folder='books/'):
 
 
 def download_image(url, filename, folder='image/'):
+    """Функция для скачивания картинок.
+        Args:
+            url (str): Cсылка на картинки, которые хочется скачать.
+            filename (str): Имя файла, с которым сохранять.
+            folder (str): Папка, куда сохранять.
+        Returns:
+            str: Путь до файла, куда сохранёны картинки.
+        """
     response = requests.get(url)
     response.raise_for_status()
     format_filename = sanitize_filename(filename)
@@ -43,58 +51,64 @@ def download_image(url, filename, folder='image/'):
 
 
 def parse_book_page(url):
+    """Функция для выявления данных из сайта tululu.org.
+        Args:
+            url (str): Cсылка на сайт, из которого выявляются данные.
+        Returns:
+            dict: Данные с сайта: название, автор, ссылка на картинку и т.д.
+        """
     book_site = 'https://tululu.org/'
     response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
-    tag = soup.find('h1').text.split('::')
-    img = soup.find('div', class_='bookimage').find('img')['src']
-    comment_tags = soup.find_all('div', class_='texts')
-    genre_tag = soup.find('span', class_='d_book').find_all('a')
-    title = tag[0].strip()
-    author = tag[1].strip()
-    site_integration = urljoin(book_site, img)
-    comment_lst = []
-    for comment in comment_tags:
-        comment_lst.append(comment.find('span').text)
-    genre_lst = []
-    for genre in genre_tag:
-        genre_lst.append(genre.text)
+    title = soup.find('h1').text.split('::')
+    path_img = soup.find('div', class_='bookimage').find('img')['src']
+    tag_comments = soup.find_all('div', class_='texts')
+    tag_genres = soup.find('span', class_='d_book').find_all('a')
+    book_title = title[0].strip()
+    author = title[1].strip()
+    image_link = urljoin(book_site, path_img)
+    comments = []
+    for comment in tag_comments:
+        comments.append(comment.find('span').text)
+    genres = []
+    for genre in tag_genres:
+        genres.append(genre.text)
     site_data = {
-        'Название': title,
-        'Автор': author,
-        'Адрес картинки': site_integration,
-        'Комментарии': comment_lst,
-        'Жанр': genre_lst
+        'name': book_title,
+        'author': author,
+        'url_image': image_link,
+        'comments': comments,
+        'genre': genres,
     }
     return site_data
 
 
 def main():
-    directory_books = 'books/'
-    directory_img = 'image/'
-    os.makedirs(directory_books, exist_ok=True)
-    os.makedirs(directory_img, exist_ok=True)
+    books_directory = 'books/'
+    img_directory = 'image/'
+    os.makedirs(books_directory, exist_ok=True)
+    os.makedirs(img_directory, exist_ok=True)
     argument_parse = argparse.ArgumentParser(description="""Скачивает книгу в количестве по значению установленному по умолчанию.
-Прописав аргументы даёт возможность установить количество книг необходимое для скачивания.
+Прописав аргументы даёт возможность установить количество книг необходимое для скачивания (начало цикла - конец цикла).
     """)
     argument_parse.add_argument('start_id', default=1, nargs='?', type=int, help='Начало цикла')
     argument_parse.add_argument('end_id', default=11, nargs='?', type=int, help='Конец цикла')
     args = argument_parse.parse_args()
 
     for i in range(args.start_id, args.end_id):
-        url_download = f"https://tululu.org/txt.php?id={i}"
-        url_book = f'https://tululu.org/b{i}/'
+        url_txt = f"https://tululu.org/txt.php?id={i}"
+        data_url = f'https://tululu.org/b{i}/'
         try:
-            data_book = parse_book_page(url_book)
-            filename = f"{i}. {data_book['Название']} - {data_book['Автор']}"
-            link_parse = urlparse(data_book['Адрес картинки'])
+            data_book = parse_book_page(data_url)
+            filename = f"{i}. {data_book['name']} - {data_book['author']}"
+            link_parse = urlparse(data_book['url_image'])
             path_separation = os.path.splitext(link_parse.path)
-            download_image(data_book['Адрес картинки'], f'{i}{path_separation[-1]}', directory_img)
-            download_txt(url_download, filename, directory_books)
-            print('Название: ', data_book['Название'])
-            print('Автор: ', data_book['Автор'])
+            download_image(data_book['url_image'], f'{i}{path_separation[-1]}', img_directory)
+            download_txt(url_txt, filename, books_directory)
+            print('Название: ', data_book['name'])
+            print('Автор: ', data_book['author'])
             print()
         except requests.exceptions.HTTPError:
             continue
