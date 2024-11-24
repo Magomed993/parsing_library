5218714ee5514e5bb90a88470fd8c48c03b473fa
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-11-25 00:34:14 +0300

    Added additional settings

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,6 +4,7 @@ import sys
 import re
 import json
 import argparse
+import time
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 from tululu import get_response, download_file, parse_book_page
@@ -16,11 +17,15 @@ def main():
     os.makedirs(img_directory, exist_ok=True)
 
     books_content = []
-    argument_parse = argparse.ArgumentParser(description="""Скачивает книги по страницам установленным по умолчанию.
-    Прописав аргументы даёт возможность установить диапазон страниц необходимое для скачивания книг (начало цикла - конец цикла).
-        """)
+    argument_parse = argparse.ArgumentParser(description="""Скачивает книги по страницам установленным по умолчанию. 
+Прописав аргументы даёт возможность установить диапазон страниц необходимое для скачивания книг (начало цикла - конец цикла), 
+    не скачивать книги, не скачивать обложки, указать путь к данным.
+    """)
     argument_parse.add_argument('start_page', default=1, nargs='?', type=int, help='Начало цикла')
     argument_parse.add_argument('end_page', default=701, nargs='?', type=int, help='Конец цикла')
+    argument_parse.add_argument('--skip_txt', default=False, action='store_true', help='Не скачивать книги')
+    argument_parse.add_argument('--skip_imgs', default=False, action='store_true', help='Не скачивать обложки')
+    argument_parse.add_argument('--dest_folder', default='', type=str, help='Путь к данным книг JSON')
     args = argument_parse.parse_args()
     for number in range(args.start_page, args.end_page):
         url = f'https://tululu.org/l55/{number}'
@@ -28,7 +33,6 @@ def main():
 
         soup = BeautifulSoup(response.text, 'lxml')
         d_book_selector = 'table.d_book'
-        # books_path = soup.find_all('table', class_='d_book')
         books_path = soup.select(d_book_selector)
         for book in books_path:
             link = book.select_one('a')['href']
@@ -40,10 +44,8 @@ def main():
             d_book_table = book_soup.select_one(d_book_selector)
             txt_link_selector = f'a[title="{book_details['name']} - скачать книгу txt"]'
             try:
-                # txt_link = d_book_table.find('a', title=f'{book_details['name']} - скачать книгу txt')
                 txt_link = d_book_table.select_one(txt_link_selector)
                 img_link_selector = 'div.bookimage img'
-                # img_link = d_book_table.find('div', class_='bookimage').find('img')['src']
                 img_link = d_book_table.select_one(img_link_selector)['src']
                 if txt_link is None:
                     raise requests.exceptions.HTTPError("Невозможно скачать, контент недоступен")
@@ -56,18 +58,17 @@ def main():
                 url_txt_parts = urlparse(link_txt_path)
                 path_img_parts = os.path.splitext(url_img_parts.path)
                 path_txt_parts = os.path.splitext(url_txt_parts.query)
-                # print(path_img_parts)
                 book_img_str_number = re.findall(r'\d+', path_img_parts[0])
                 book_txt_number = int(re.findall(r'\d+', path_txt_parts[0])[0])
                 if book_img_str_number:
                     book_img_number = int(book_img_str_number[0])
                 else:
                     book_img_number = f'nopic'
-                # print(book_img_str_number)
                 file_name = f'{book_txt_number}: {book_details['name']} - {book_details['author']}.txt'
-                download_file(book_link_txt_response, file_name, books_directory)
-                download_file(book_link_img_response, f'{book_img_number}{path_img_parts[-1]}', img_directory)
-                # book_txt_path = urlparse(book_link_txt_response)
+                if not args.skip_txt:
+                    download_file(book_link_txt_response, file_name, books_directory)
+                if not args.skip_imgs:
+                    download_file(book_link_img_response, f'{book_img_number}{path_img_parts[-1]}', img_directory)
                 book_content = {
                     'title': book_details['name'],
                     'author': book_details['author'],
@@ -78,11 +79,14 @@ def main():
                 }
                 books_content.append(book_content)
                 print(book_link_response.url)
+            except requests.exceptions.ConnectionError:
+                print('Соединение прервано. Скрипт продолжает работу', file=sys.stderr)
+                time.sleep(1800)
             except requests.exceptions.HTTPError as error:
                 print(f'{book_link_response.url}: {error}', file=sys.stderr)
                 continue
     book_content_json = json.dumps(books_content)
-    with open('book_content.json', 'w') as my_file:
+    with open(os.path.join(args.dest_folder ,'book_content.json'), 'w') as my_file:
         my_file.write(book_content_json)
 
 
